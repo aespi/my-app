eed03a5728162f1f283db5bec946a568d80ac074
aespi <spsingh86900@gmail.com>
2020-01-20 18:17:18 +0530

    resolve top bar error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductAlertsComponent,
     TopBarComponent,
     ProductDetailsComponent,
+    CartComponent,
    
   ],
   imports: [
@@ -24,6 +26,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      
     ])
   ],
   providers: [],
